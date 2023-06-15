635ff28db54ab13c9fde2dc9ff294c593f128fd4
SetthuL <langesiphosethu@gmail.com>
2023-06-15 08:14:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unixstd.h>
 /**
- * main - A C program that prints wit fputs
- * Return: 1
+ * main - A C program that prints wit fput
+ * Return: 1 (The program runs a success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
