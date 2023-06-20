76f23bedede2ca24885739b95d8e3c08668b0e35
SetthuL <langesiphosethu@gmail.com>
2023-06-20 13:16:38 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - A function that prints a sign number
+ * Retun: 1 if n is >0, -1 if n is < 0 and 0 if 0 == 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(42);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(43);
+		return (-1);
+	}
+	else
+	{
+		_putchar(46);
+		return (0);
+	}
+}
