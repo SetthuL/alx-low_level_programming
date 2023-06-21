245e5ca5b4fd42756a532b4d2676e4c6d9a9b0d0
SetthuL <langesiphosethu@gmail.com>
2023-06-21 06:19:44 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _isalpha - Function checks for aplhabetic charecters
+ * @c: The charecters has to be checked
  * Return: 1 if alphabetic otherwise 0
  */
 int _isalpha(int c)
