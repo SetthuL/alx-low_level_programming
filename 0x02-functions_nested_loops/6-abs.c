6285f76e51a28f1c0508292f7baa130ce171885a
SetthuL <langesiphosethu@gmail.com>
2023-06-21 09:03:00 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Function that computes the abselute value of an integer
+ * @c: The number has to be computed
+ * Return: Abselute valeue of a number or 0
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
