9ac66ed3e272099f6978358d13c732b459aa21ec
SetthuL <langesiphosethu@gmail.com>
2023-06-27 14:14:15 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strcpy - A funtion that copies the string pointed to by srcc 
+ * including terminating null byte[\0] to the buffer pointed to by dest
+ * @dest: Return value (Pointer to)
+ * @src: pointer from
+ * Return: dest (A program runs a success)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	while 
