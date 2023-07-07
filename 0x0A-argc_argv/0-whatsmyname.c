f09e996c25591ad5599f082bddd127cf383b66f2
SetthuL <langesiphosethu@gmail.com>
2023-07-07 07:12:36 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name, followed by new line
+ * @argc: Input
+ * @argv: Input
+ * Return: 0 (A program runs a success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
