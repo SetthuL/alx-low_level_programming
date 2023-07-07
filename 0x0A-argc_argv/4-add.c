ed21854984e08d4cf714561d743ff356e7a85a51
SetthuL <langesiphosethu@gmail.com>
2023-07-07 09:21:27 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - A program that adds positive numbers followed by new line
+ * @argc: Input
+ * @argv: Input
+ * Return: 0 (A program runs a success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int sl;
+	unsigned int b, sum = 0;
+	char *h;
+
+	if (argc > 1)
+	{
+		for (sl = 1; sl < argc; sl++)
+		{
+			h = argv[sl];
+
+			for (b = 0; b < strlen(h); b++)
+			{
+				if (h[b] < 48 || h[b] > 57)
+				{
+					printf("Error");
+					printf("\n");
+				}
+			}
+			sum += atoi(h);
+			h++;
+		}
+		printf("%d", sum);
+		printf("\n");
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
+
