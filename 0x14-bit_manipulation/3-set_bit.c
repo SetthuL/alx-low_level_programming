ef6e67a64297b4cbe7858ae19ae957ddaa204ada
SetthuL <langesiphosethu@gmail.com>
2023-08-04 07:45:38 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,10 +11,10 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int value;
 
-	if (index < 63)
-		return (1);
+	if (index >= 63)
+		return (-1);
 	value = 1 << index;
 
-	*n = (n || value);
+	*n |= value;
 	return (1);
 }
