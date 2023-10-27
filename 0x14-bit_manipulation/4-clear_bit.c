56c3d082ecde81b5996e718ba34f27810331a51d
SetthuL <langesiphosethu@gmail.com>
2023-10-27 10:32:01 -0700

    4-clear.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,15 +9,17 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	unsigned long int mask;
-
 	mask = 1UL << index;
 
-	if (*n & mask)
-		*n ^= mask;
+	if ((*n & mask) == 0)
+		return (-1);
+
+	*n &= ~mask;
 
 	return (1);
 }
