5b6812dd10a945a1f992545c8b9f20b5b6667b1c
SetthuL <langesiphosethu@gmail.com>
2023-11-03 13:15:26 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,24 +12,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-        int p, q, length = 0;
+	int p, q, length = 0;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        if (text_content != NULL)
-        {
+	if (text_content != NULL)
+	{
                 for (length = 0; text_content[length];)
-                        length++;
-        }
+			length++;
+	}
 
-        p = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-        q = write(p, text_content, length);
+	p = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	q = write(p, text_content, length);
 
-        if (p == -1 || q == -1)
-                return (-1);
+	if (p == -1 || q == -1)
+		return (-1);
 
-        close(p);
+	close(p);
 
-        return (1);
+	return (1);
 }
