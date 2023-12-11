4f2400cae0f08f90c4662475d052cc0b975744dc
SetthuL <langesiphosethu@gmail.com>
2023-12-11 12:46:44 -0800

    strmp.h file

--- 0x18-dynamic_libraries/3-strcmp.c
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -3,21 +3,22 @@
  * _strcmp - A function that compare string values
  * @s1: input value
  * @s2: input value
- *
  * Return: s1[sethu] - s2[sethu]
  */
+
 int _strcmp(char *s1, char *s2)
 {
-		int sethu;
+	int sethu;
 
-		sethu = 0;
-		while (s1[sethu] != '\0' && s2[sethu] != '\0')
+	sethu = 0;
+
+	while (s1[sethu] != '\0' && s2[sethu] != '\0')
+	{
+		if (s1[sethu] != s2[sethu])
 		{
-			if (s1[sethu] != s2[sethu])
-			{
-				return (s1[sethu] - s2[sethu]);
-			}
-			sethu++;
-			}
-			return (0);
+			return (s1[sethu] - s2[sethu]);
+		}
+		sethu++;
+	}
+	return (0);
 }
---
+++ 0x18-dynamic_libraries/3-strmp.h
@@ -0,0 +1,6 @@
+#ifndef STRMP_H
+#define STRMP_H
+
+int _strcmp(const char *s1, const char *s2);
+
+#endif
